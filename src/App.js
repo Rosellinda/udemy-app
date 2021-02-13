a6b4ea04834148d7efccd704b1201d8d9b8c336e
Rosellinda <64804606+Rosellinda@users.noreply.github.com>
2021-02-14 04:35:53 +0800

    Working with Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  
+
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am Roselle!</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m Roselle Burlasa'));
+    return (
+      <div className="App">
+        <h1>Hi, I am Roselle!</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name="Roselle" age="28"/>
+        <Person name="Kevin"  age="29" >My Hobbies: Racing</Person>
+        <Person name="Zelda" age="26"/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m Roselle Burlasa'));
   }
 }
 
